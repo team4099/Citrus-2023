4a6f4135cf9fcfaa1fb86aadcdaeaf9a00dea0a6
Saraansh Wadkar <saraanshwadkar@gmail.com>
2023-07-27 21:17:20 -0400

    add constants

--- src/main/kotlin/com/team4099/robot2023/config/constants/ArmConstants.kt
+++ src/main/kotlin/com/team4099/robot2023/config/constants/ArmConstants.kt
@@ -1,4 +1,15 @@
 package com.team4099.robot2023.config.constants
 
+import org.team4099.lib.units.base.amps
+import org.team4099.lib.units.derived.degrees
+import org.team4099.lib.units.derived.gearRatio
+import org.team4099.lib.units.derived.volts
+
 object ArmConstants {
+  val SPROCKET_RATIO = ((12.0 / 48.0) * (24.0 * 48.0)).gearRatio
+
+  val VOLTAGE_COMPENSATION = 12.volts
+  val ARM_CURRENT_LIMIT = 60.amps // TODO TUNE
+
+  val ABSOLUTE_ENCODER_OFFSET = 0.degrees  // No actual value can be determined
 }
