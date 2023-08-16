b97cd5ae3b0b4341fddd9f944abffc0c9aac6370
NeonCoal <52942449+NeonCoal@users.noreply.github.com>
2023-08-15 20:09:44 -0400

    Update ElevatorIOSim.kt

--- src/main/kotlin/com/team4099/robot2023/subsystems/elevator/ElevatorIOSim.kt
+++ src/main/kotlin/com/team4099/robot2023/subsystems/elevator/ElevatorIOSim.kt
@@ -1,5 +1,35 @@
 package com.team4099.robot2023.subsystems.elevator
 
+import com.team4099.lib.math.clamp
+import com.team4099.robot2023.config.constants.Constants
+import com.team4099.robot2023.config.constants.ElevatorConstants
+import com.team4099.robot2023.subsystems.falconspin.MotorChecker
+import com.team4099.robot2023.subsystems.falconspin.MotorCollection
+import com.team4099.robot2023.subsystems.falconspin.SimulatedMotor
+import com.team4099.robot2023.util.ElevatorSim
+import edu.wpi.first.math.system.plant.DCMotor
+import edu.wpi.first.wpilibj.simulation.BatterySim
+import edu.wpi.first.wpilibj.simulation.RoboRioSim
+import org.team4099.lib.controller.PIDController
+import org.team4099.lib.units.base.Length
+import org.team4099.lib.units.base.Meter
+import org.team4099.lib.units.base.amps
+import org.team4099.lib.units.base.celsius
+import org.team4099.lib.units.base.inSeconds
+import org.team4099.lib.units.base.meters
+import org.team4099.lib.units.derived.DerivativeGain
+import org.team4099.lib.units.derived.ElectricalPotential
+import org.team4099.lib.units.derived.IntegralGain
+import org.team4099.lib.units.derived.ProportionalGain
+import org.team4099.lib.units.derived.Volt
+import org.team4099.lib.units.derived.asDrivingOverDriven
+import org.team4099.lib.units.derived.inVolts
+import org.team4099.lib.units.derived.volts
+import org.team4099.lib.units.perSecond
+
 object ElevatorIOSim : ElevatorIO {
-  private val VAL_NAME = ElevatorSim()
+  val elevatorSim: ElevatorSim =
+    ElevatorSim(
+      
+    )
 }
